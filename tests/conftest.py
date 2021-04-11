8b52872162ed14377fbb35eaf9c5544a25d7e95b
Lina Apfelbaum <mevakeret.heshbonot@gmail.com>
2021-04-11 17:08:04 +0500

    Homework1. Implemented Circle subclass

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
 import pytest
 from src.Triangle import Triangle
+from src.Circle import Circle
 
 @pytest.fixture()
 def default_triangle():
     return Triangle(3, 4, 5)
+
+@pytest.fixture()
+def default_circle():
+    return Circle(5)
+
---
+++ tests/test_circle.py
@@ -0,0 +1,23 @@
+import pytest
+
+def test_create_circle(default_circle):
+    """Check the figure is created"""
+    assert default_circle.radius == 5
+    assert default_circle.angles == 0
+
+def test_calculate_area(default_circle):
+    """Check the area is calculated"""
+    assert default_circle.area == 79
+
+def test_calculate_perimeter(default_circle):
+    """Check the perimeter is calculated"""
+    assert default_circle.perimeter == 31
+
+def test_add_area(default_circle, default_triangle):
+    """Check two figures are summed up"""
+    assert default_circle.add_area(default_triangle) == 85
+
+def test_add_area_error(default_circle):
+    """Check an error raises if argument 'figure' doesn't belong to 'Figure' class"""
+    with pytest.raises(ValueError):
+        default_circle.add_area('hey')
